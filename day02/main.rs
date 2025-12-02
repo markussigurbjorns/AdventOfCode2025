faa13dcbfea17d17381ef94bb8781f5dd7108c4e
markussigurbjorns <markussigurbjorns@gmail.com>
2025-12-02 19:31:10 +0100

    day2

--- day02/main.rs
+++ day02/main.rs
@@ -43,16 +43,16 @@ pub unsafe fn read_file(path: *const c_char) -> *mut c_char {
     buf
 }
 
-pub unsafe fn parse_number(mut p: *const c_char) -> (i32, *const c_char) {
-    let mut val: i32 = 0;
+pub unsafe fn parse_number(mut p: *const c_char) -> (u64, *const c_char) {
+    let mut val: u64 = 0;
 
     loop {
-        let ch = ptr::read(p);
-        if ch == b'\n' as i8 || ch == 0 { break; }
+        let ch = *p;
+        if ch == b'-' as i8 || ch == 0 || ch == b',' as i8 { break; }
         if ch < b'0' as i8 || ch > b'9' as i8 { break; }
 
-        let d = (ch.saturating_sub(b'0' as i8)) as i32;
-        val = val.wrapping_mul(10).wrapping_add(d);
+        let d = (ch - b'0' as i8) as u64;
+        val = val * 10 + d;
 
         p = p.add(1);
     }
@@ -61,15 +61,133 @@ pub unsafe fn parse_number(mut p: *const c_char) -> (i32, *const c_char) {
 }
 
 
-pub unsafe fn add_num(num:i32) -> i32 {
-
-    num+4_i32
+pub unsafe fn num_to_string(n:u64) -> *mut c_char {
+    let buf_size: usize = 21;
+    let buf = malloc(buf_size) as *mut c_char;
+    if buf.is_null() {
+        return ptr::null_mut();
+    }
+    snprintf(buf, buf_size, c"%llu".as_ptr(), n);
+    buf
 }
 
-pub unsafe fn process_part1(buf: *const c_char) -> i32 {
+pub unsafe fn get_str_len(str: *mut c_char) -> usize {
+    strlen(str)
+}
+
+pub unsafe fn is_invalid(str: *mut c_char, len: usize) -> bool {
+    let half = len / 2;
+
+    let left  = malloc(half + 1) as *mut c_char;
+    let right = malloc(half + 1) as *mut c_char;
+
+    if left.is_null() || right.is_null() {
+        return false;
+    }
+
+    for i in 0..half {
+        ptr::write(left.add(i), ptr::read(str.add(i)));
+    }
+    ptr::write(left.add(half), 0); 
+
+    for i in 0..half {
+        ptr::write(right.add(i), ptr::read(str.add(half + i)));
+    }
+    ptr::write(right.add(half), 0); 
+
+    if strcmp(left, right) == 0 {
+        return true;
+    }
+    return false
+
+
+}
+
+pub unsafe fn process_part1(buf: *const c_char) -> u64 {
+    let mut p = buf;
+    let mut sum:u64 = 0;
+        
+    while *p != 0 {
+        
+        let (val1, new_p) = parse_number(p);
+        let (val2, new_new_p) = parse_number(new_p.add(1));
+
+        for d in val1..=val2 {
+            let num_str = num_to_string(d);
+            let str_len = get_str_len(num_str);
+            if str_len % 2 == 0 {
+                if is_invalid(num_str, str_len) {
+                    sum +=d
+                }
+            }
+            free(num_str as *mut c_void);
+        }
+
+        p = new_new_p;
+        p = p.add(1);
+    }
+    sum
     
-    add_num(5_i32)
 }
+
+pub unsafe fn is_invalid_p2(str: *mut c_char, len: usize) -> bool {
+    let half = len/2;
+    let mut res: bool = false;
+    for i in 1..=half {
+        if len % i != 0 {
+            continue;
+        }
+        let mut fail: bool = false;
+        let mut p = str;
+        let to_cmp = malloc(i + 1) as *mut c_char;
+        for j in 0..i {
+            ptr::write(to_cmp.add(j), ptr::read(str.add(j)));
+            ptr::write(to_cmp.add(i), 0);
+        }
+    
+        for _k in 0..(len/i){
+            if strncmp(to_cmp, p, i as usize) != 0 {
+                fail = true;
+                break;
+            }
+            p = p.add(i);
+        }
+        if !fail {
+            res = true;
+        }
+
+        free(to_cmp as *mut c_void);
+    
+    }
+    res
+
+}
+
+pub unsafe fn process_part2(buf: *const c_char) -> u64 {
+    let mut p = buf;
+    let mut sum:u64 = 0;
+        
+    while *p != 0 {
+        
+        let (val1, new_p) = parse_number(p);
+        let (val2, new_new_p) = parse_number(new_p.add(1));
+
+        for d in val1..=val2 {
+            let num_str = num_to_string(d);
+            let str_len = get_str_len(num_str);
+            if is_invalid_p2(num_str, str_len) {
+                sum +=d
+            }
+            free(num_str as *mut c_void);
+        }
+
+        p = new_new_p;
+        p = p.add(1);
+    }
+    sum
+    
+}
+
 
 #[no_mangle]
 pub unsafe extern "C" fn main(argc: i32, argv: *mut *mut c_char) -> i32 {
@@ -82,8 +200,10 @@ pub unsafe extern "C" fn main(argc: i32, argv: *mut *mut c_char) -> i32 {
     let text = read_file(arg1);
 
     if !text.is_null() {
-        // START HERE
-        process_part1(text);
+        let res1 = process_part1(text);
+        printf(c"%llu\n".as_ptr(), res1);
+        let res2 = process_part2(text);
+        printf(c"%llu\n".as_ptr(), res2);
     }
     0
 }
@@ -100,11 +220,17 @@ mod libc {
     extern "C" {
         pub fn abort() -> !;
         pub fn printf(format: *const c_char, ...) -> c_int;
+        pub fn snprintf(s: *mut c_char, n: usize, format: *const c_char, ...) -> c_int;
         pub fn fopen(filename: *const c_char, mode: *const c_char) -> *mut FILE;
         pub fn fclose(file: *mut FILE) -> c_int;
         pub fn fseek(stream: *mut FILE, offset: c_long, whence: c_int) -> c_int;
         pub fn fread(ptr: *mut c_void, size: usize, nobj: usize, stream: *mut FILE) -> usize;
         pub fn ftell(stream: *mut FILE) -> c_long;
         pub fn malloc(size: usize) -> *mut c_void;
+        pub fn free(p: *mut c_void);
+        pub fn strlen(cs: *const c_char) -> usize;
+        pub fn strcmp(cs: *const c_char, ct: *const c_char) -> c_int;
+        pub fn strncmp(cs: *const c_char, ct: *const c_char, n: usize) -> c_int;
+
     }
 }
