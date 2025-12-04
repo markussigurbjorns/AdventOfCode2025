7bedda1a3f2322d728036c7af64f8013462f48ca
markussigurbjorns <markussigurbjorns@gmail.com>
2025-12-04 17:31:01 +0100

    day4

--- day04/main.rs
+++ day04/main.rs
@@ -130,6 +130,80 @@ pub unsafe fn process_part1(buf: *const c_char) -> i32 {
 }
 
 
+pub unsafe fn process_part2(buf: *const c_char) -> i32 {
+    let mut p = buf;
+    let mut rolls: i32 = 0;
+    let mut fp: FloorPlan = new_floor_plan();
+    let mut l: i32 = 0;
+    let mut h: i32 = 0;
+    while *p != 0 {
+        if *p == b'\n' as i8 {
+            fp.length = l;
+            h += 1;
+        } else {
+            da_append(&mut fp.slots, *p);
+        }
+        if fp.length < 1 {
+            l +=1;
+        }
+        p = p.add(1);
+    }
+    fp.height = h;
+
+    let deltas = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+    loop {
+        let mut to_be_removed:Array<usize> = zeroed();
+        for r in 0..fp.height {
+            for c in 0..fp.length {
+                let a = idx(r,c,fp.height);
+                let slot_a = fp.slots.items.add(a as usize);
+                if *slot_a != b'@' as i8 {continue}
+                let mut count = 0;
+                let mut pick_up = true;
+                for (dr,dc) in deltas {
+                    let nr = r + dr;
+                    let nc = c + dc;
+                    if nr < 0 || nr >= fp.height || nc < 0 || nc >= fp.length {
+                        continue;
+                    }
+                    let b = idx(nr, nc, fp.height);
+                    let slot_b = fp.slots.items.add(b as usize);
+
+                    if *slot_a == *slot_b {
+                        count +=1;
+                        if count == 4 {
+                            pick_up = false;
+                            break;
+                        }
+                    }
+                }
+                if pick_up {
+                    da_append(&mut to_be_removed, a as usize);
+                    rolls += 1;
+                }
+            }
+        }
+        if to_be_removed.count == 0 {break;}
+        else {
+            while to_be_removed.count != 0 {
+                let rem = da_pop(&mut to_be_removed).unwrap();
+                da_replace(&mut fp.slots, rem, '.' as i8);
+            }
+        }
+    }
+    rolls
+
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn main(argc: i32, argv: *mut *mut c_char) -> i32 {
     
@@ -143,6 +217,8 @@ pub unsafe extern "C" fn main(argc: i32, argv: *mut *mut c_char) -> i32 {
     if !text.is_null() {
         let res1 = process_part1(text);
         printf(c"%d\n".as_ptr(), res1);
+        let res2 = process_part2(text);
+        printf(c"%d\n".as_ptr(), res2);
 
     }
     0
@@ -178,11 +254,11 @@ pub mod libc {
     }
 }
 
-pub mod da { 
+
+pub mod da {
     use crate::libc;
     use core::ptr;
     use core::ffi::*;
-
 
     #[repr(C)]
     #[derive(Clone, Copy)]
@@ -193,24 +269,79 @@ pub mod da {
     }
 
     pub unsafe fn da_append<T>(da: *mut Array<T>, item: T) {
-        if (*da).count >= (*da).capacity {
-            if (*da).capacity == 0 {
-                (*da).capacity = 256;
+        let da_ref = &mut *da;
+
+        if da_ref.count >= da_ref.capacity {
+            if da_ref.capacity == 0 {
+                da_ref.capacity = 256;
             } else {
-                (*da).capacity *= 2;
+                da_ref.capacity *= 2;
             }
-            (*da).items = libc::realloc_items((*da).items, (*da).capacity);
+            da_ref.items = libc::realloc_items(da_ref.items, da_ref.capacity);
         }
-        *((*da).items.add((*da).count)) = item;
-        (*da).count += 1;
+
+        *da_ref.items.add(da_ref.count) = item;
+        da_ref.count += 1;
+    }
+
+    pub unsafe fn da_pop<T>(da: *mut Array<T>) -> Option<T> {
+        let da_ref = &mut *da;
+
+        if da_ref.count == 0 {
+            return None;
+        }
+
+        da_ref.count -= 1;
+        let ptr = da_ref.items.add(da_ref.count);
+
+        Some(ptr::read(ptr))
+    }
+
+    pub unsafe fn da_remove<T>(da: *mut Array<T>, index: usize) -> Option<T> {
+        let da_ref = &mut *da;
+
+        if index >= da_ref.count {
+            return None;
+        }
+
+        let ptr = da_ref.items.add(index);
+        let removed = ptr::read(ptr);
+
+        let num_to_move = da_ref.count - index - 1;
+        if num_to_move > 0 {
+            ptr::copy(ptr.add(1), ptr, num_to_move);
+        }
+
+        da_ref.count -= 1;
+
+        Some(removed)
+    }
+
+    pub unsafe fn da_replace<T>(da: *mut Array<T>, index: usize, item: T) -> Option<T> {
+        let da_ref = &mut *da;
+
+        if index >= da_ref.count {
+            return None;
+        }
+
+        let ptr = da_ref.items.add(index);
+
+        let old = ptr::read(ptr);
+        ptr::write(ptr, item);
+
+        Some(old)
     }
 
     pub unsafe fn da_destroy<T>(da: *mut Array<T>) {
-        libc::free((*da).items as *mut c_void);
-        (*da).items = ptr::null_mut();
-        (*da).count = 0;
-        (*da).capacity = 0;
+        let da_ref = &mut *da;
+
+        for i in 0..da_ref.count {
+            ptr::drop_in_place(da_ref.items.add(i));
+        }
+
+        libc::free(da_ref.items as *mut c_void);
+        da_ref.items = ptr::null_mut();
+        da_ref.count = 0;
+        da_ref.capacity = 0;
     }
 }
-
-
